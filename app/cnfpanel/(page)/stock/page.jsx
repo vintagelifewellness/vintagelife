2a698d473c489395caf53c447e332085f3b84864
Parmjeet Rathore <teamparmjeet@gmail.com>
2026-09-03 17:27:31 +0530

    4554

--- app/cnfpanel/(page)/stock/page.jsx
+++ app/cnfpanel/(page)/stock/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import CnfStock from '@/components/CnfStock'
+import CnfStock from '@/components/CnfStockforuser'
 import { useSession } from "next-auth/react"
 
 export default function page() {
